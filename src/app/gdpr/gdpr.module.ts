aa2f3c71d7b8caa7515778349e208eff28a19603
Sergii.Shestakov <sshest78@gmail.com>
2022-08-02 12:33:49 +0200

    Add app_bootstrap_listener injection token for rendering a GDPR modal component

--- src/app/gdpr/gdpr.module.ts
+++ src/app/gdpr/gdpr.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {APP_BOOTSTRAP_LISTENER, ComponentFactoryResolver, ComponentRef, NgModule, ViewContainerRef} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 
@@ -9,6 +9,20 @@ import { ModalComponent } from './modal/modal.component';
   imports: [
     CommonModule
   ],
-  entryComponents: [ModalComponent]
+  entryComponents: [ModalComponent],
+  providers: [
+    {
+      multi: true,
+      provide: APP_BOOTSTRAP_LISTENER,
+      useFactory: (resolver: ComponentFactoryResolver) => {
+        return (componentRef: ComponentRef<any>) => {
+          const factory = resolver.resolveComponentFactory(ModalComponent);
+          const viewContainerRef = componentRef.injector.get(ViewContainerRef);
+          viewContainerRef.createComponent(factory);
+        };
+      },
+      deps: [ComponentFactoryResolver]
+    }
+  ]
 })
 export class GdprModule { }
